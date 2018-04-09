49fc98aa70ea72c61d792564a08a2e9a38802e7f
poverfield <30505202+poverfield@users.noreply.github.com>
2018-04-09 18:52:32 -0400

    Update initial_data_pull.R
    add in directory for write.csv

--- initial_data_pull.R
+++ initial_data_pull.R
@@ -17,6 +17,10 @@ df = matrix(data = df, ncol = 8, byrow = FALSE) # recreate data matrix
 colnames(df) = c("time","open","high","low","close","vwap","colume","count")
 df = df[,1:5]
 
+# set directory
+dir = 'home/pi/Desktop/files'
+setwd(dir)
+
 # write out file
 write.csv(df, file = 'xbt_data.csv')
 
