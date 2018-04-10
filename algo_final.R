f8d0fba4a9ad65ce3aeaaa3127e541fa7f43483a
poverfield <30505202+poverfield@users.noreply.github.com>
2018-04-10 15:54:53 -0400

    Update algo_final.R
    put data pull in 'exists' function so the chunk will run in while loop until retrieve errors.  Script will not miss any more intervals

--- algo_final.R
+++ algo_final.R
@@ -7,24 +7,28 @@ suppressWarnings(library(TTR))
 # wait 55 seconds before run
 Sys.sleep(55)
 
-# query data from Kraken
-# download data from Kraken
-pair = "XXBTZUSD" # 0r "XXRPZUSD"
-interval = '30'
-base.url = "https://api.kraken.com/0/public/OHLC"
-url <- paste0(base.url, "?", "pair=", pair, "&interval=", interval)
 
-
-# interval = minute
-ohlc.out <- jsonlite::fromJSON(url) 
-#time.stamp = ohlc.out[[2]]$last
-
-# create data table + change to numerical
-df = ohlc.out[[2]]$XXBTZUSD
-df = as.numeric(df) # change to numeric
-df = matrix(data = df, ncol = 8, byrow = FALSE) # recreate data matrix
+# try and download data until 'df exists
+while(!exists('df', mode = 'numeric')){
+  # download data from Kraken
+  pair = "XXBTZUSD" # 0r "XXRPZUSD"
+  interval = '30'
+  base.url = "https://api.kraken.com/0/public/OHLC"
+  url <- paste0(base.url, "?", "pair=", pair, "&interval=", interval)
+    
+    
+  # interval = minute
+  ohlc.out <- jsonlite::fromJSON(url) 
+    #time.stamp = ohlc.out[[2]]$last
+    
+  # create data table + change to numerical
+  df = ohlc.out[[2]]$XXBTZUSD
+  df = as.numeric(df) # change to numeric
+  df = matrix(data = df, ncol = 8, byrow = FALSE) # recreate data matrix
+}
 colnames(df) = c("time","open","high","low","close","vwap","colume","count")
 df = df[,1:5]
+
 
 # set directory
 dir = '/home/pi/Desktop/files'
