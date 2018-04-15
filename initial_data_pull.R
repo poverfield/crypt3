9d34211162c46329ad1145063670c4f16d08db5f
poverfield <30505202+poverfield@users.noreply.github.com>
2018-04-15 16:45:04 -0400

    Add files via upload
    new algorithm based on guppy bands.  python script should be identical to before, only R scripts should change.  call eth data not xbt

--- initial_data_pull.R
+++ initial_data_pull.R
@@ -1,26 +1,23 @@
-
 # download data from Kraken
-pair = "XXBTZUSD" # 0r "XXRPZUSD"
+pair = "XETHZUSD" # 0r "XXRPZUSD"
 interval = '30'
 base.url = "https://api.kraken.com/0/public/OHLC"
 url <- paste0(base.url, "?", "pair=", pair, "&interval=", interval)
-  
-  
+
+
 # interval = minute
 ohlc.out <- jsonlite::fromJSON(url) 
-  #time.stamp = ohlc.out[[2]]$last
-  
+#time.stamp = ohlc.out[[2]]$last
+
 # create data table + change to numerical
-df = ohlc.out[[2]]$XXBTZUSD
+df = ohlc.out[[2]]$XETHZUSD
 df = as.numeric(df) # change to numeric
 df = matrix(data = df, ncol = 8, byrow = FALSE) # recreate data matrix
 colnames(df) = c("time","open","high","low","close","vwap","colume","count")
 df = df[,1:5]
 
 # set directory
-dir = 'home/pi/Desktop/files'
-setwd(dir)
-
+# dir = 
+# setwd = dir
 # write out file
-write.csv(df, file = 'xbt_data.csv')
-
+write.csv(df, file = 'eth_data.csv')
