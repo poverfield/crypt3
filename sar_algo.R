5f46c55622b6e516c6d765af2f9c65245ace4a90
poverfield <30505202+poverfield@users.noreply.github.com>
2018-04-24 22:14:40 -0400

    Add files via upload
    sar algo -> pull in data, produce buy and sell signals
    trade_script3.py --> read in trade_hist.csv and execute trade if warrented
    trade_log.txt --> file to log trade information,  to be emailed weekly

---
+++ sar_algo.R
@@ -0,0 +1,141 @@
+# SAR Algorithm
+
+# load packages
+suppressWarnings(library(quantmod))
+suppressWarnings(library(TTR))
+
+# wait 50 seconds to run so it'll run at minutes 29:50 min and 59:50
+Sys.sleep(50)
+
+# Sar Paramaters
+v = 0.031
+
+# create dummy ohlc.out to compare lengths
+ohlc.out = 1
+while(length(ohlc.out) == 1){ # run until the data downloads
+  # query data
+  # download data from Kraken
+  pair = "XETHZUSD" # 0r "XXRPZUSD"
+  interval = '30'
+  base.url = "https://api.kraken.com/0/public/OHLC"
+  url <- paste0(base.url, "?", "pair=", pair, "&interval=", interval)
+  
+  
+  # interval = minute
+  ohlc.out <- jsonlite::fromJSON(url) 
+}
+
+# create data table
+df = ohlc.out[[2]]$XETHZUSD
+df = as.numeric(df) # change to numeric
+df = matrix(data = df, ncol = 8, byrow = FALSE) # recreate data matrix
+colnames(df) = c("time","open","high","low","close","vwap","colume","count")
+df = df[,1:5]
+
+
+# set up directory on rasbperry
+dir = '/home/pi/Desktop/files'
+setwd(dir)
+
+# read in aggregate data
+df_hist = read.csv('eth_data.csv')
+df_hist = df_hist[,-1]
+
+# number of new intervals (720 = 0, 719 = 1, 718 = 2... etc)
+id = which(df[,1] == df_hist[nrow(df_hist),1])
+
+if(id < 720){
+  dat = rbind(df_hist, df[(id+1):nrow(df),])
+  write.csv(dat, file = 'eth_data.csv')
+  print(dat[nrow(dat),1] - dat[nrow(dat)-1,1])
+} else {
+  dat = df_hist
+}
+
+
+# save dat for algo
+# if dat has more than 720 rows -- only use last 720 rows for algorithm
+if(nrow(dat) > 1000){
+  dat.origin = dat[(nrow(dat)-999):nrow(dat),]
+} else {
+  dat.origin = dat
+}
+
+
+########## SAR ALGO  ########## 
+
+sar = SAR(dat.origin[,3:4], accel = c(v,v))
+
+dat = dat.origin
+# signal = buy when price > sar
+buy = which(dat[,5] > sar)
+sell = which(dat[,5] < sar)
+
+if(sell[1] < buy[1]){
+  sell = sell[(which(sell > buy[1])[1]):length(sell)]
+}
+
+hold_length = max(c(length(buy), length(sell)))
+  
+b = buy[1]
+s = sell[1]
+  
+# clean indices
+for(i in 2:hold_length){
+  b = c(b, buy[which(buy > s[i-1])[1]])
+  s = c(s, sell[which(sell > b[i])[1]])
+}
+  
+# remove NAs
+buy = b[!is.na(b)]
+sell = s[!is.na(s)]
+
+# read in trade history
+trade_hist = read.csv('trade_hist.csv')
+trade_hist = as.matrix(trade_hist[,-1])
+
+# write out signal
+signal = max(buy,sell)
+trade = matrix(data = 0, ncol = 3, nrow = 1) # create matrix to fill with trade information
+if(signal != nrow(dat)){
+  trade = NA
+} else if (signal %in% buy){
+  trade[1,1] = as.character(dat[signal,1]) # date
+  trade[1,2] = 'buy'  # signal
+  trade[1,3] = dat[signal,5] # price
+  trade_hist = rbind(trade_hist, trade)
+  # write out new file
+  write.csv(trade_hist, file = 'trade_hist.csv')
+  # write out plot
+  png(filename="plot.png")
+  plot(x = 1:nrow(dat), y = dat[,5], type = 'l', xlab = '30-min interval', ylab = 'ETH Price', main = 'ETH Price and Historic Trades')
+  lines(x = 1:nrow(dat), y = gup[,7], col = 2, lty = 2)
+  lines(x = 1:nrow(dat), y = gup[,12], col = 2, lty = 2)
+  abline(v= buy, col = 3, lty = 2)
+  abline(v = sell, col = 2, lty = 2)
+  dev.off()
+} else if (signal %in% sell){
+  trade[1,1] = as.character(dat[signal,1]) # date
+  trade[1,2] = 'sell'  # signal
+  trade[1,3] = dat[signal,5] # price
+  trade_hist = rbind(trade_hist, trade)
+  # write out new file
+  write.csv(trade_hist, file = 'trade_hist.csv')
+  # write out plot
+  png(filename="plot.png")
+  plot(x = 1:nrow(dat), y = dat[,5], type = 'l', xlab = '30-min interval', ylab = 'ETH Price', main = 'ETH Price and Historic Trades')
+  lines(x = 1:nrow(dat), y = gup[,7], col = 2, lty = 2)
+  lines(x = 1:nrow(dat), y = gup[,12], col = 2, lty = 2)
+  abline(v= buy, col = 3, lty = 2)
+  abline(v = sell, col = 2, lty = 2)
+  dev.off()
+}
+
+
+
+# write out current date/time and price
+sink('current_price.txt')
+cat(as.character(dat[nrow(dat),1]))
+cat('\n')
+cat(dat[nrow(dat),5])
+sink()
---
+++ trade_script3.py
@@ -0,0 +1,206 @@
+# crypto trade script using krakenex
+# Load libraries
+import csv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import datetime
+import os
+import krakenex
+import time
+
+# Set working directory in raspberry
+path = '/home/pi/Desktop/files'
+os.chdir(path)
+
+# Stop loss
+stop_loss = .02
+take_profit = .08
+
+# Read in account information
+f = open('account.txt') # read in account.txt
+f_lines = f.readlines()
+email = f_lines[0].split(': ',1)[1][0:len(f_lines[0].split(': ',1)[1])-1] # email address (send and recieve)
+password = f_lines[1].split(': ',1)[1][0:len(f_lines[1].split(': ',1)[1])-1] # email password
+api_key = f_lines[2].split(': ',1)[1][0:len(f_lines[2].split(': ',1)[1])-1] # kraken api key
+api_secret = f_lines[3].split(': ',1)[1] # kraken api secret
+f.close()
+
+# Email Function
+def send_email(trade): # function with input 'buy/sell/close'
+    sender = email
+    receiver = email
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Trade'
+    msg['From'] = sender
+    msg['To'] = receiver
+    file = 'plot.png'
+
+    now = str(datetime.datetime.now())
+    now_date = now[0:16]
+    
+    message = 'Time stamp: ' + now_date + '\n\n' + 'Action: ' + trade + '\n\n' + 'price: ' + str(current_price)
+    msg.attach(MIMEText(message))
+    attachment = MIMEBase('application', 'octet-stream')
+    attachment.set_payload(open(file, 'rb').read())
+    encoders.encode_base64(attachment)
+    attachment.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
+    msg.attach(attachment)
+    s = server = smtplib.SMTP('smtp.gmail.com:587') #smtp.gmail.com:587
+    s.starttls()
+    s.login(sender, password)
+    s.sendmail(sender, receiver, msg.as_string())
+    s.quit()
+
+# Order function
+def order_func(action, pair, volume, volume2, stop_price, take_price):
+    print('trading...')
+    if(action == 'buy'):
+        action2 = 'sell'
+    elif(action == 'sell'):
+        action2 = 'buy'
+    order = k.query_private('AddOrder',
+                                 {'pair': pair,
+                                  'type': action,
+                                  'ordertype': 'market',
+                                  'volume': volume,
+                                  'leverage': '2:1'})
+    time.sleep(2)
+    order2 = k.query_private('AddOrder',
+                                 {'pair': pair,
+                                  'type': action2,
+                                  'ordertype': 'take-profit',
+                                  'volume': volume2,
+                                  'price': take_price,
+                                  'leverage': '2:1'})
+    time.sleep(2)
+    order3 = k.query_private('AddOrder',
+                                 {'pair': pair,
+                                  'type': action2,
+                                  'ordertype': 'stop-loss',
+                                  'volume': volume2,
+                                  'price': stop_price,
+                                  'leverage': '2:1'})                           
+    print('trade complete.')
+    # send email
+    send_email(action)
+    # write trade info to trade_log.txt
+    f = open('trade_log.txt','a')
+    f.write('\n\n' + str(datetime.datetime.now())[0:16])
+    f.write('; Error: ' + str(order['error']))
+    f.write('; Action: ' + str(order['result']['descr']['order']))
+    f.write('; ID: ' + str(order['result']['txid'])[2:][:-2])
+    f.write('\nTake_profit-->')
+    f.write(' Error: ' + str(order2['error']))
+    f.write('; Action: ' + str(order2['result']['descr']['order']))
+    f.write('; ID: ' + str(order2['result']['txid'])[2:][:-2])
+    f.write('\nstop_loss-->')
+    f.write(' Error: ' + str(order3['error']))
+    f.write('; Action: ' + str(order3['result']['descr']['order']))
+    f.write('; ID: ' + str(order3['result']['txid'])[2:][:-2])  
+    f.close()
+    # write trade id to trade_id.txt
+    #f = open('trade_id.txt','w')
+    #f.write(str(order['result']['txid'])[2:][:-2])
+    #f.close()
+
+# Close open trades
+def close_open_func():
+    open_order = k.query_private('OpenOrders') # get open orders
+    while len(open_order['result']['open']) > 0: 
+        open_order_id = list(open_order['result']['open'])[0] # id
+        k.query_private('CancelOrder', {'txid': open_order_id}) # cancel order
+        open_order = k.query_private('OpenOrders') # re_open order to get new length
+    time.sleep(2) # sleep 2 second to allow to cancel orders
+
+
+# read in trade_hist
+f = open('trade_hist.csv')
+f_reader = csv.reader(f)
+f_data = list(f_reader)
+trade_sig = f_data[len(f_data)-1][2] # most recent trade
+trade_price = float(f_data[len(f_data)-1][3]) # most recent price
+trade_date = f_data[len(f_data)-1][1]
+f.close()
+
+# Read in current price
+f = open('current_price.txt')
+f_lines = f.readlines()
+current_date = f_lines[0] # read in date + time intervals
+current_date = current_date[0:(len(current_date)-1)] # remove '\n' from current_date
+current_price = float(f_lines[1]) # price
+f.close()
+
+# Connect to Kraken
+k = krakenex.API(key = api_key, secret = api_secret)
+
+# Available volume for new trade
+balance = k.query_private('Balance')
+balance = balance['result'] # clean 'balance'
+usd = float(balance['ZUSD'])
+volume_open = round(((usd*.5)/current_price),4)
+
+# recent trade information
+close_order = k.query_private('ClosedOrders',{'trades': 'true','start':'1523363400'})
+recent_id = get_recent_id()
+prev_action = close_order['result']['closed'][recent_id]['descr']['type']
+
+# Get prev_volume from open position
+open_order = k.query_private('OpenOrders')
+if(len(open_order['result']['open']) > 0):
+    open_order_id = list(open_order['result']['open'])[0]
+    prev_vol = open_order['result']['open'][open_order_id]['vol']
+
+
+
+# Execute Trade
+if(len(open_order['result']['open']) > 1):
+    if(trade_date == current_trade and trade_sig == 'buy'):
+        close_open_func() # cancel open orders
+        action = 'buy'
+        pair = 'XETHZUSD'
+        volume = str(float(prev_vol)*2) # use 2x volume to close previous trade and open new trade
+        volume2 = prev_vol
+        stop_price = str(current_price*(1-stop_loss))
+        take_price = str(current_price*(1+take_profit))
+        order_func(action, pair, volume, volume2, stop_price, take_price)
+        print('buy reverse')
+    elif(trade_date == current_trade and trade_sig == 'sell'):
+        close_open_func() # cancel open orders
+        action = 'sell'
+        pair = 'XETHZUSD'
+        volume = str(float(prev_vol)*2) # use 2x volume to close previous trade and open new trade
+        volume2 = prev_vol
+        stop_price = str(current_price*(1+stop_loss))
+        take_price = str(current_price*(1-take_profit))
+        order_func(action, pair, volume,  volume2, stop_price, take_price)
+        print('sell reverse')
+elif(len(open_order['result']['open']) < 2):
+    if(trade_date == current_trade and trade_sig == 'buy'):
+        close_open_func() # cancel open orders
+        action = 'buy'
+        pair = 'XETHZUSD'
+        volume = volume_open # use volume from account
+        volume2 = volume
+        stop_price = str(current_price*(1-stop_loss))
+        take_price = str(current_price*(1+take_profit))
+        order_func(action, pair, volume,  volume2, stop_price, take_price)
+        print('buy new')
+    elif(trade_date == current_trade and trade_sig == 'sell'):
+        close_open_func() # cancel open orders
+        action = 'sell'
+        pair = 'XETHZUSD'
+        volume = volume_open # use volume from account
+        volume2 = volume # close volumes
+        stop_price = str(current_price*(1+stop_loss))
+        take_price = str(current_price*(1-take_profit))
+        order_func(action, pair, volume,  volume2, stop_price, take_price)
+        print('sell new')
+else:
+    print('Hold.')
+
+print('Script complete.')
+
